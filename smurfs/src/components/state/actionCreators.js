b1c83908780fedbe881cfbeb317c8dd344c7bd19
mark-marshall <mark.marshallgp@gmail.com>
2019-03-08 08:35:10 +0000

    [MVP] Added all action creators for GET and POST smurfs

---
+++ smurfs/src/components/state/actionCreators.js
@@ -0,0 +1,57 @@
+import axios from 'axios';
+
+import * as types from './actionTypes';
+
+const smurfURL = 'http://localhost:3333/smurfs';
+
+export const getSmurfAsync = () => dispatch => {
+  dispatch(spinnerOn());
+  axios
+    .get(smurfURL)
+    .then(smurfs => {
+      dispatch(getSmurfs(smurfs.data));
+      dispatch(spinnerOff());
+    })
+    .catch(error => {
+      dispatch(throwError(error.message));
+    });
+};
+
+export const addSmurfAsync = smurf => dispatch => {
+  dispatch(spinnerOn());
+  axios
+    .post(smurfURL, smurf)
+    .then(smurfs => {
+      dispatch(getSmurfs(smurfs.data));
+      dispatch(spinnerOff());
+    })
+    .catch(error => {
+      dispatch(throwError(error.message));
+    });
+};
+
+export function getSmurfs(smurfs) {
+  return {
+    type: types.GET_SMURFS,
+    payload: smurfs,
+  };
+}
+
+export function spinnerOn() {
+  return {
+    type: types.SPINNER_ON,
+  };
+}
+
+export function spinnerOff() {
+  return {
+    type: types.SPINNER_OFF,
+  };
+}
+
+export function throwError(error) {
+  return {
+    type: types.THROW_ERROR,
+    payload: error,
+  };
+}
